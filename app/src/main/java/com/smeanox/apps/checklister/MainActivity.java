0a22be3b7d2c920685e3dd55e5597ad758616f35
SmBe19 <git@schmidb.ch>
2016-04-11 23:22:39 +0200

    implement basic list

--- app/src/main/java/com/smeanox/apps/checklister/MainActivity.java
+++ app/src/main/java/com/smeanox/apps/checklister/MainActivity.java
@@ -3,17 +3,18 @@ package com.smeanox.apps.checklister;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-	public final static String EXTRA_MESSAGE = "com.smeanox.app.MESSAGE";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +27,7 @@ public class MainActivity extends AppCompatActivity {
 		fab.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
+				addMenuItem(view.getRootView());
 			}
 		});
 	}
@@ -48,17 +48,48 @@ public class MainActivity extends AppCompatActivity {
 
 		//noinspection SimplifiableIfStatement
 		if (id == R.id.action_settings) {
+			Intent intent = new Intent(this, SettingsActivity.class);
+			startActivity(intent);
 			return true;
 		}
 
 		return super.onOptionsItemSelected(item);
 	}
 
-	public void sendMessage(View view) {
-		Intent intent = new Intent(this, DisplayMessageActivity.class);
-		EditText editText = (EditText) findViewById(R.id.edit_message);
-		String message = editText.getText().toString();
-		intent.putExtra(EXTRA_MESSAGE, message);
-		startActivity(intent);
+	public void addMenuItem(View view) {
+		LayoutInflater layoutInflater = getLayoutInflater();
+		LinearLayout checkBox = (LinearLayout) layoutInflater.inflate(R.layout.checklist_item, null);
+		((CheckBox) checkBox.findViewById(R.id.checklistItem)).setText(R.string.new_item);
+
+		LinearLayout layout = (LinearLayout) findViewById(R.id.mainLayout);
+		layout.addView(checkBox);
+	}
+
+	private View oldEditCheckbox;
+
+	public void editCheckbox(View view) {
+		View parent = (View) view.getParent();
+		if(oldEditCheckbox == parent){
+			endEditCheckbox(parent);
+			return;
+		}
+		if(oldEditCheckbox != null){
+			endEditCheckbox(oldEditCheckbox);
+		}
+		startEditCheckbox(parent);
+	}
+
+	private void startEditCheckbox(View view) {
+		((EditText) view.findViewById(R.id.checklistEdit)).setText(((TextView) view.findViewById(R.id.checklistItem)).getText());
+		view.findViewById(R.id.checklistItem).setVisibility(View.GONE);
+		view.findViewById(R.id.checklistEdit).setVisibility(View.VISIBLE);
+		oldEditCheckbox = view;
+	}
+
+	private void endEditCheckbox(View view){
+		((TextView) view.findViewById(R.id.checklistItem)).setText(((EditText) view.findViewById(R.id.checklistEdit)).getText().toString());
+		view.findViewById(R.id.checklistItem).setVisibility(View.VISIBLE);
+		view.findViewById(R.id.checklistEdit).setVisibility(View.GONE);
+		oldEditCheckbox = null;
 	}
 }
