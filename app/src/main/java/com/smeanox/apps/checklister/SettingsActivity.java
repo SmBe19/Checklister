bef6c54b62908e672a98434f2fb36f89bab1309a
SmBe19 <git@schmidb.ch>
2016-04-14 19:01:13 +0200

    Improve Settings

--- app/src/main/java/com/smeanox/apps/checklister/SettingsActivity.java
+++ app/src/main/java/com/smeanox/apps/checklister/SettingsActivity.java
@@ -1,10 +1,12 @@
 package com.smeanox.apps.checklister;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -23,6 +25,21 @@ public class SettingsActivity extends AppCompatActivity {
 	}
 
 	public void resetDb(View view) {
+		new AlertDialog.Builder(this)
+				.setIcon(android.R.drawable.ic_dialog_alert)
+				.setTitle(R.string.ResetDialogTitle)
+				.setMessage(R.string.ResetDialogMessage)
+				.setPositiveButton(R.string.ResetDialogPositive, new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						resetDbForReal();
+					}
+				})
+				.setNegativeButton(R.string.ResetDialogNegative, null)
+				.show();
+	}
+
+	private void resetDbForReal() {
 		ChecklistDB.get().resetDb();
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
 			finishAffinity();
